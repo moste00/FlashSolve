b301197869a9a4082d9d426f36dc098906a54c53
Makrion <miklaziz3112000@gmail.com>
2023-04-08 16:57:25 +0200

    fix: optimizing-hashbits-choices

--- src/main/sample.cs
+++ src/main/sample.cs
@@ -139,7 +139,7 @@ public static class Sample {
             }
         }
         else if (strategy == "UNI_HASH_XOR") {
-            const int hashSize = 13;
+            const int hashSize = 12;
             Console.WriteLine("limit = "+ limit+".");
             Console.WriteLine("hash Size = "+ hashSize + " bits.");
             Console.WriteLine("hash groups = "+ Math.Pow(2, hashSize) + " solutions.");
@@ -184,7 +184,7 @@ public static class Sample {
                         namesToValues[constName].Add(
                             Int32.Parse(con.Value.ToString())
                         );
-                        // execlude it from next
+                        // exclude it from next
                         allVariablesHaveNewValues = ctx.MkAnd(ctx.MkEq(hash, con.Value));
                     }
                     else if (constName.Contains("hash") || constName == "input")
@@ -214,13 +214,44 @@ public static class Sample {
         }
         otherStopWatch.Stop();
         Console.WriteLine("Done!");
-        Console.WriteLine($"Found {numSols} Solutions in {stopwatch.Elapsed.Seconds} seconds.");
-        Console.WriteLine($"Total main loop time is {otherStopWatch.Elapsed.Seconds} seconds.");
+        Console.WriteLine($"Found {numSols} Solutions in {stopwatch.Elapsed.Minutes} minutes and {stopwatch.Elapsed.Seconds} seconds.");
+        Console.WriteLine($"Total main loop time is {otherStopWatch.Elapsed.Minutes} minutes and {stopwatch.Elapsed.Seconds} seconds.");
         WriteSolsJson(String.Join("",args), namesToValues);
     }
 
-    private static List<uint> generate_hash_bits_idx(int bitsCount, int min, int max)
+    private static void print_list(List<uint> mylist)
     {
+        var str_line = "{ ";
+        for (int i = 0; i < mylist.Count(); i++)
+        {
+            if (i > 0)
+                str_line += ",";
+            str_line += mylist[i] + " ";
+        }
+
+        str_line += "}";
+        Console.WriteLine("new List<uint> " + str_line);
+    }
+    private static List<uint> generate_hash_bits_idx(int bitsCount, int min, int max, int current_idx)
+    {
+        List<List<uint>> memoized_hashBits = new List<List<uint>>{ 
+            new List<uint> { 17 ,73 ,31 ,3 ,38 ,53 ,37 ,57 ,91 ,34 ,58 ,66 ,20 ,88 ,86 ,92 ,36 ,6 ,35 ,70 ,83 ,2 ,24 ,41 ,9 ,54 ,45 ,42 ,44 ,10 ,32 ,56 },
+            new List<uint> { 8 ,78 ,15 ,26 ,95 ,32 ,65 ,34 ,51 ,1 ,27 ,71 ,76 ,88 ,7 ,54 ,21 ,80 ,92 ,10 ,30 ,77 ,74 ,37 ,25 ,93 ,91 ,61 ,87 ,72 ,11 ,56 },
+            new List<uint> { 60 ,28 ,20 ,82 ,71 ,59 ,81 ,3 ,42 ,27 ,7 ,50 ,46 ,10 ,1 ,79 ,58 ,30 ,26 ,19 ,57 ,11 ,88 ,17 ,34 ,80 ,62 ,86 ,43 ,73 ,4 ,6 },
+            new List<uint> { 82 ,69 ,47 ,20 ,72 ,32 ,62 ,64 ,68 ,51 ,61 ,58 ,25 ,34 ,16 ,86 ,70 ,22 ,56 ,6 ,76 ,50 ,79 ,18 ,44 ,26 ,90 ,53 ,88 ,8 ,59 ,14 },
+            new List<uint> { 22 ,58 ,2 ,66 ,84 ,7 ,12 ,28 ,0 ,95 ,10 ,57 ,80 ,23 ,27 ,59 ,21 ,50 ,39 ,65 ,38 ,30 ,9 ,44 ,60 ,87 ,53 ,37 ,1 ,90 ,47 ,54 },
+            new List<uint> { 3 ,8 ,9 ,77 ,78 ,30 ,61 ,59 ,74 ,11 ,2 ,18 ,1 ,71 ,93 ,37 ,35 ,27 ,42 ,49 ,65 ,19 ,48 ,52 ,38 ,56 ,24 ,67 ,72 ,57 ,70 ,64 },
+            new List<uint> { 55 ,11 ,93 ,32 ,46 ,43 ,42 ,63 ,71 ,87 ,61 ,5 ,48 ,10 ,36 ,7 ,92 ,49 ,53 ,77 ,83 ,29 ,52 ,95 ,19 ,28 ,76 ,80 ,30 ,67 ,59 ,90 },
+            new List<uint> { 69 ,64 ,27 ,93 ,18 ,28 ,74 ,30 ,85 ,41 ,86 ,8 ,61 ,3 ,47 ,70 ,50 ,91 ,71 ,87 ,56 ,24 ,67 ,34 ,94 ,21 ,49 ,42 ,33 ,68 ,82 ,63 },
+            new List<uint> { 95 ,44 ,24 ,91 ,87 ,38 ,81 ,76 ,25 ,33 ,14 ,1 ,59 ,93 ,37 ,56 ,21 ,57 ,8 ,90 ,47 ,13 ,43 ,54 ,6 ,12 ,77 ,45 ,69 ,70 ,2 ,55 },
+            new List<uint> { 9 ,76 ,43 ,8 ,85 ,31 ,89 ,29 ,4 ,58 ,80 ,13 ,52 ,77 ,30 ,47 ,65 ,33 ,57 ,84 ,59 ,5 ,79 ,66 ,36 ,18 ,81 ,70 ,17 ,63 ,82 ,0 },
+            new List<uint> { 60 ,47 ,29 ,74 ,72 ,87 ,4 ,63 ,41 ,21 ,93 ,46 ,9 ,49 ,80 ,82 ,20 ,1 ,30 ,2 ,10 ,59 ,71 ,43 ,0 ,31 ,44 ,16 ,45 ,61 ,17 ,23 },
+            new List<uint> { 10 ,68 ,75 ,37 ,31 ,85 ,23 ,82 ,41 ,76 ,7 ,45 ,33 ,83 ,81 ,11 ,57 ,36 ,66 ,30 ,32 ,1 ,40 ,52 ,46 ,14 ,4 ,53 ,62 ,8 ,73 ,0 },
+        };
+
+        if (current_idx < 12)
+            return memoized_hashBits[current_idx];
+
         List<uint> hashBits = new List<uint>();
         if (bitsCount > (max - min))
         {
@@ -237,6 +268,7 @@ public static class Sample {
             else
                 i--;
         }
+        print_list(hashBits);
         return hashBits;
     }
 
@@ -251,7 +283,7 @@ public static class Sample {
         for (int idx = 0; idx < hashSize; idx++)
         {
             // gen new hashBits idxes
-            List<uint> hashBits = generate_hash_bits_idx(32, 0, 96);
+            List<uint> hashBits = generate_hash_bits_idx(32, 0, 96, idx);
             // create a constant for the new hash
             var hash_i = (BitVecExpr)ctx.MkConst("hash"+idx, bvbitHashType);
             // xor first 2-bits
