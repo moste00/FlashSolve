d6c831e3fa93cc967f588158e2cf1db7df970f82
Ahmed-Emad10 <ahmemad10@gmail.com>
2023-06-19 15:50:56 +0300

    ConstraintPrototype and 4 classes for constraintexpr

---
+++ src/parser/ast/SvUniqueness.cs
@@ -0,0 +1,16 @@
+﻿namespace flashsolve.parser.ast;
+
+public class SvUniqueness : SvConstraint.Expr
+{
+    private List<Tuple<SvExpr,SvExpr?>> _expr;
+    
+    public SvUniqueness()
+    {
+        _expr = new List<Tuple<SvExpr,SvExpr?>>();
+    }
+    
+    public void Add(Tuple<SvExpr,SvExpr?> record)
+    {
+        _expr.Add(record);
+    }
+}
